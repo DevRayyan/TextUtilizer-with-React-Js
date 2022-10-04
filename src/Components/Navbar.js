c82706188acb185e51236d67a5aaca326d5e644c
Mohammad Taha <m.taha10045@gmail.com>
2022-10-04 12:32:20 -0700

    solved router errors and add navigation links in navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Navbar(props) {
   const colorPicker = () => {
@@ -23,8 +24,9 @@ let code = document.getElementById('colorcode').value;
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item mx-2 ms-4 ">{props.home}</li>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 col justify-content-center">
+            <li className="nav-item mx-2 ms-4 ">{props.active==false?<Link className="nav-link" to="/">Home</Link>:<Link className="nav-link fw-bold" to="/">Home</Link>}</li>
+            <li className="nav-item mx-2 ms-4">{props.active==false?<Link className="nav-link fw-bold" to="/about">About</Link>:<Link className="nav-link" to="/about">About</Link>}</li>
           </ul>
           <div className="form-check form-switch">
             <input onClick={props.togglemode}
